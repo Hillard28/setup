964cce8d1ba50903db0615aed389bdd689500f12
Ryan Gilland <ryangilland@gmail.com>
2024-06-30 18:43:56 -0400

    Update surface.sh

--- scripts/surface.sh
+++ scripts/surface.sh
@@ -1,5 +1,5 @@
 # Install linux-surface kernel
 sudo chroot /mnt/sysimage
 dnf config-manager --add-repo=https://pkg.surfacelinux.com/fedora/linux-surface.repo
-dnf --disablerepo=updates install --allowerasing kernel-surface iptsd libwacom-surface
-dnf install surface-secureboot
+dnf --disablerepo=updates install --allowerasing kernel-surface iptsd libwacom-surface -y
+dnf install surface-secureboot -y
