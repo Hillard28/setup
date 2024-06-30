673818d7bc3cc6cf5faae4d4de6dd0261eb15e95
Ryan Gilland <ryangilland@gmail.com>
2024-06-30 19:12:48 -0400

    Update modifications.sh

--- scripts/modifications.sh
+++ scripts/modifications.sh
@@ -2,6 +2,11 @@
 dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf config-manager --enable fedora-cisco-openh264
 
+# Install multimedia
+dnf swap ffmpeg-free ffmpeg --allowerasing
+dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf install @sound-and-video
+
 # Remove unwanted packages
 dnf remove unoconv @libreoffice rhythmbox -y
 
