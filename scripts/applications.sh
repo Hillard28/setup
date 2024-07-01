8640b39911555709f822bc09d3ab7be5c857804f
Hillard28 <ryangilland@gmail.com>
2024-07-01 15:18:52 -0400

    Update applications.sh

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -3,6 +3,8 @@
 dnf install gparted -y
 dnf install inkscape -y
 flatpak install flathub org.onlyoffice.desktopeditors -y
+flatpak run org.onlyoffice.desktopeditors --system-title-bar
+kill $(pgrep DesktopEditors)
 flatpak install flathub com.mattjakeman.ExtensionManager -y
 
 # Install configuration software
