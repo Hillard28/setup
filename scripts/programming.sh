8d017a57b8ebe6a5703c090d3c6a81f913d0a3c2
Ryan Gilland <ryangilland@gmail.com>
2024-07-01 10:00:42 -0400

    Update programming.sh

--- scripts/programming.sh
+++ scripts/programming.sh
@@ -20,3 +20,9 @@ dnf install gh --repo gh-cli -y
 git config --global user.name "Hillard28"
 git config --global user.email "ryangilland@gmail.com"
 gh auth login
+
+# Install vscode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update
+dnf install code
