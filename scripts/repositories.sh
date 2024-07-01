3ac910ac5b7358903b103e1812b3b7e7b9b7fb4e
Ryan Gilland <ryangilland@gmail.com>
2024-07-01 08:40:54 -0400

    Update repositories.sh

--- scripts/repositories.sh
+++ scripts/repositories.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Configure additional repositories
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf config-manager --enable fedora-cisco-openh264
+dnf groupupdate core -y
