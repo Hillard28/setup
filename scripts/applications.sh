b85e6d3df85f776e87716e1e135bb39f557a59bc
Ryan Gilland <ryangilland@gmail.com>
2024-07-01 08:45:10 -0400

    Update applications.sh

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -2,5 +2,8 @@
 # Install applications
 dnf install gparted -y
 dnf install inkscape -y
+flatpak install flathub org.onlyoffice.desktopeditors -y
+flatpak install flathub com.mattjakeman.ExtensionManager -y
 
+# Install configuration software
 dnf install dconf-editor gnome-tweaks -y
