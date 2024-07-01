291a0c4d2d4070dd719dc48cf46ebd19997d01a6
Ryan Gilland <ryangilland@gmail.com>
2024-07-01 14:18:38 -0400

    Update multimedia.sh

--- scripts/multimedia.sh
+++ scripts/multimedia.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Install multimedia
-dnf swap ffmpeg-free ffmpeg --allowerasing
-dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf swap ffmpeg-free ffmpeg --allowerasing -y
+dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 dnf install @sound-and-video -y
