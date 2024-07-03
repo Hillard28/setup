195b0db301fa4351586b506b7d13058f57f8a462
Hillard28 <ryangilland@gmail.com>
2024-07-02 22:56:39 -0400

    Update scripts

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -9,15 +9,15 @@ git checkout f40
 cd ..
 mv ~/rpmbuild/gnome-control-center/gnome-control-center.spec ~/rpmbuild/SPECS/gnome-control-center.spec
 rm -R gnome-control-center
+cd ~/rpmbuild/SPECS/
+
+perl -p0i -e 's/%if\s0%{\?fedora}(\r\n|\r|\n)\s+-Ddistributor_logo=%{_datadir}\/pixmaps\/fedora_logo_med\.png\s+\\(\r\n|\r|\n)\s+-Ddark_mode_distributor_logo=%{_datadir}\/pixmaps\/fedora_whitelogo_med\.png\s+\\(\r\n|\r|\n)%endif(\r\n|\r|\n)//g' gnome-control-center.spec
+perl -p0i -e 's/%if\s0%{\?rhel}(\r\n|\r|\n)\s+-Ddistributor_logo=%{_datadir}\/pixmaps\/fedora-logo\.png\s+\\(\r\n|\r|\n)\s+-Ddark_mode_distributor_logo=%{_datadir}\/pixmaps\/system-logo-white\.png\s+\\(\r\n|\r|\n)%endif(\r\n|\r|\n)//g' gnome-control-center.spec
 
 cd ~/rpmbuild/SOURCES/
 curl -LO https://download.gnome.org/sources/gnome-control-center/46/gnome-control-center-46.2.tar.xz
 
 cd ~/
-
-xdg-open rpmbuild/SPECS/gnome-control-center.spec
-
-# AT THIS POINT, YOU NEED TO EDIT THE SPEC FILE TO REMOVE LOGO REFERENCES
 
 dnf builddep rpmbuild/SPECS/gnome-control-center.spec
 rpmbuild -ba rpmbuild/SPECS/gnome-control-center.spec
@@ -32,9 +32,8 @@ createrepo Projects/irepo
 rpm -i --reinstall Projects/irepo/packages/gnome-control-center-46.2-1.fc40.x86_64.rpm
 
 # Add minimize and maximize options
-gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
 
 # Edit os-release settings
-xdg-open /usr/lib/os-release
+sed -i 's/LOGO=fedora-logo-icon/LOGO=infinity-logo-256/g' /usr/lib/os-release
 
-# AT THIS POINT CAN EDIT LOGO PATH IN OS-RELEASE
--- scripts/modifications.sh
+++ scripts/modifications.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Add Pop!_OS Shell
+dnf install gnome-shell-extension-pop-shell xprop
+
 # Change boot logo
 plymouth-set-default-theme -R igrt
 
