c2f52ae3c03bee7eab35a307697c8c61aaa715ee
Ryan Gilland <ryangilland@gmail.com>
2024-07-10 16:47:19 -0400

    Update VS Code script

--- scripts/programming.sh
+++ scripts/programming.sh
@@ -25,8 +25,8 @@ git config --global user.email "ryangilland@gmail.com"
 gh auth login
 
 # Install vscode
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf check-update -y
 dnf install code -y
 
