051dc3ee612ccf7204f77bd861f94d36c206212c
Ryan Gilland <ryangilland@gmail.com>
2024-07-11 18:14:14 -0400

    Add framebuffer

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -2,4 +2,5 @@
 # Add minimize and maximize options
 gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
 gsettings set org.gnome.login-screen logo '/usr/share/icons/infinity-logo-104-dark.png'
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']
 dconf update
