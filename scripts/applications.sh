be743a22441c0a8aab582166e33c010bf3278f83
Ryan Gilland <ryangilland@gmail.com>
2024-07-15 10:37:02 -0400

    Remove Inkscape installation

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Install applications
 dnf install gparted -y
-dnf install inkscape -y
 flatpak install flathub org.onlyoffice.desktopeditors -y
 flatpak run org.onlyoffice.desktopeditors --system-title-bar
 kill $(pgrep DesktopEditors)
