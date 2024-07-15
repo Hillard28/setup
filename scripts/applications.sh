b379b9dd86d5855cf4e1db9af9637cae358513bd
Ryan Gilland <ryangilland@gmail.com>
2024-07-15 10:41:25 -0400

    Move dconf-editor to Flathub

--- scripts/applications.sh
+++ scripts/applications.sh
@@ -4,10 +4,12 @@ dnf install gparted -y
 flatpak install flathub org.onlyoffice.desktopeditors -y
 flatpak run org.onlyoffice.desktopeditors --system-title-bar
 kill $(pgrep DesktopEditors)
-flatpak install flathub com.mattjakeman.ExtensionManager -y
 
-# Install configuration software
-dnf install dconf-editor gnome-tweaks -y
+# Install dconf editor
+flatpak install flathub ca.desrt.dconf-editor
+
+# Install Gnome Tweaks
+dnf install gnome-tweaks -y
 
 # Install utilities
 dnf install openssl
