7a875ef54b469bc80db574d79ade40e27f1218e9
Ryan Gilland <ryangilland@gmail.com>
2024-07-15 13:06:26 -0400

    Expand instructions

--- scripts/surface.sh
+++ scripts/surface.sh
@@ -9,8 +9,11 @@ sudo mount --rbind /sys /mnt/root/sys
 sudo mount --rbind /run /mnt/root/run
 sudo chroot /mnt/root /usr/bin/bash
 
+# Once chroot done
 dnf config-manager --add-repo=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 dnf --disablerepo=updates install --allowerasing kernel-surface iptsd libwacom-surface -y
+
+# At the moment only works with Grub
 dnf install surface-secureboot -y
 
 # After first boot in fedora
