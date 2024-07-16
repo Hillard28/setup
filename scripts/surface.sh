53030cfed495efe9f82992a0efc179f62a70506c
Ryan Gilland <ryangilland@gmail.com>
2024-07-15 21:51:36 -0400

    Update surface.sh

--- scripts/surface.sh
+++ scripts/surface.sh
@@ -1,16 +1,5 @@
 # Install linux-surface kernel immediately after installing fedora
 sudo chroot /mnt/sysimage
-
-# If netinstall, boot into Workstation Live USB
-sudo mount /dev/nvme0n1p8 /mnt
-sudo mount --rbind /dev /mnt/root/dev
-sudo mount --rbind /proc /mnt/root/proc
-sudo mount --rbind /sys /mnt/root/sys
-sudo mount --rbind /run /mnt/root/run
-sudo mount --rbind /boot /mnt/root/boot
-sudo chroot /mnt/root /usr/bin/bash
-
-# Once chroot done
 dnf config-manager --add-repo=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 dnf --disablerepo=updates install --allowerasing kernel-surface iptsd libwacom-surface -y
 
