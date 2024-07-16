4c6ce3893e7d70134dee9ed92f06bf1ca3088039
Ryan Gilland <ryangilland@gmail.com>
2024-07-15 22:34:28 -0400

    Add VS Code again

--- scripts/programming.sh
+++ scripts/programming.sh
@@ -26,3 +26,9 @@ rpm --import "https://miktex.org/download/key"
 curl -L -o /etc/yum.repos.d/miktex.repo https://miktex.org/download/fedora/40/miktex.repo
 dnf update -y
 dnf install miktex -y
+
+# Visual Studio Code
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update
+dnf install code # or code-insider
