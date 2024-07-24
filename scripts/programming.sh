b23f0c1c06a9ae8d3e6bf0249727226bea21ce12
Ryan Gilland <ryangilland@gmail.com>
2024-07-24 10:34:15 -0400

    Update programming.sh

--- scripts/programming.sh
+++ scripts/programming.sh
@@ -12,15 +12,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # Install RPM dev tools
 sudo dnf install rpm-build rpmdevtools createrepo_c -y
 
-# Install github and login
-sudo dnf install 'dnf-command(config-manager)' -y
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo dnf install gh --repo gh-cli -y
-
-git config --global user.name "Hillard28"
-git config --global user.email "ryangilland@gmail.com"
-gh auth login
-
 # Install MikTeX
 sudo rpm --import "https://miktex.org/download/key"
 sudo curl -L -o /etc/yum.repos.d/miktex.repo https://miktex.org/download/fedora/40/miktex.repo
@@ -32,3 +23,12 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 sudo dnf check-update
 sudo dnf install code # or code-insider
+
+# Install github and login
+sudo dnf install 'dnf-command(config-manager)' -y
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install gh --repo gh-cli -y
+
+git config --global user.name "Hillard28"
+git config --global user.email "ryangilland@gmail.com"
+gh auth login
