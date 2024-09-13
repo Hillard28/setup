4d0111fd588d780d260aa82894c7347c7dfe95d7
Hillard28 <ryangilland@gmail.com>
2024-09-13 13:45:30 -0400

    Add Perl modules

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -51,6 +51,12 @@ sudo curl -L -o /etc/yum.repos.d/miktex.repo https://miktex.org/download/fedora/
 sudo dnf update -y
 sudo dnf install miktex -y
 
+# Install Perl packages for compiling LaTeX
+sudo dnf install 'perl(File::Copy)' -y
+sudo dnf install 'perl(Time::HiRes)' -y
+sudo dnf install 'perl(Unicode::Normalize)' -y
+sudo dnf install 'perl(sigtrap)' -y
+
 # Visual Studio Code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
