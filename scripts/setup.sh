417729dc800d10e0bf6dcb6781b89acf012243a1
Hillard28 <ryangilland@gmail.com>
2024-09-18 10:14:03 -0400

    Fix automated Microsoft Edge install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -75,7 +75,7 @@ gh auth login
 # Install Microsoft Edge
 #sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc # Duplicate from VSCode install
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
-sudo dnf install microsoft-edge-stable
+sudo dnf install microsoft-edge-stable -y
 
 # Install onlyoffice
 sudo flatpak install flathub org.onlyoffice.desktopeditors -y
