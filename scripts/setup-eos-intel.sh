567e0cc3d5fa15a2e61b803a07a0d16e999c02a1
Ryan Gilland <ryangilland@gmail.com>
2024-11-16 20:01:20 -0500

    Start EOS setup script

---
+++ scripts/setup-eos-intel.sh
@@ -0,0 +1,17 @@
+
+yay -S base-devel clang llvm cmake
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. ~/.bashrc
+. ~/.bash_profile
+
+yay -S github-cli
+git config --global user.email "ryangilland@gmail.com"
+git config --global user.name "Ryan Gilland"
+gh auth login
+
+yay -S visual-studio-code-bin
+yay -S texlive-latexrecommended texlive-latexextra texlive-fontsrecommended texlive-fontsextra texlive-xetex
+
+yay -S dropbox dolphin-plugins
+
+yay -S inter-font
