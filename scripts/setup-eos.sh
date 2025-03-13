f41d92d86361f3c2d19a63e10c29c81a38a64e3c
Ryan Gilland <ryangilland@gmail.com>
2025-03-13 13:50:33 -0400

    Update install script and directories

--- scripts/setup-eos.sh
+++ scripts/setup-eos.sh
@@ -4,6 +4,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Update installed packages
 yay -Syyu
 
+# Install missing gnome packages
+yay -S gnome --needed
+
 # Copy background, profile, and icon folders
 sudo mv $SCRIPT_DIR/../home/userprofile/Pictures/Background ~/Pictures
 sudo mv $SCRIPT_DIR/../home/userprofile/Pictures/Documents ~/Pictures
@@ -11,9 +14,10 @@ sudo mv $SCRIPT_DIR/../home/userprofile/Pictures/Profile ~/Pictures
 sudo mv $SCRIPT_DIR/../home/userprofile/.local/share/icons ~/.local/share
 
 # Install Inter and Segoe UI font packages
-yay -S inter-font nerd-fonts
-sudo mv $SCRIPT_DIR/../usr/share/fonts/segoe-ui-fonts /usr/share/fonts/segoe-ui-fonts
-fc-cache -fv
+sudo mv $SCRIPT_DIR/../home/userprofile/.local/share/fonts ~/.local/share
+yay -S inter-font
+# yay -S nerd-fonts
+fc-cache -f
 
 # Install and configure ZRAM package
 yay -S zram-generator
@@ -28,8 +32,6 @@ curl -fsSL https://install.julialang.org | sh
 . ~/.bashrc
 . ~/.bash_profile
 . ~/.profile
-
-yay -S execstack
 
 # Install Pyenv
 curl -fsSL https://pyenv.run | bash
@@ -53,36 +55,40 @@ yay -S gtk2 ncurses5-compat-libs
 # Install TeX Live packages 
 yay -S texlive-latexrecommended texlive-latexextra texlive-fontsrecommended texlive-fontsextra texlive-fontutils texlive-formatsextra texlive-xetex texlive-binextra texlive-pictures texlive-mathscience texlive-bibtexextra perl-yaml-tiny perl-file-homedir
 
+# Install Gnome VFS packages
+yay -S gvfs gvfs-afc gvfs-dnssd gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd
+
+# Install workspace packages
+yay -S python-gpgme
+yay -S dropbox
+yay -S slack-desktop
+yay -S libreoffice-fresh
+yay -S nautilus-dropbox
+
+# Install configuration packages
+yay -S extension-manager gnome-tweaks dconf-editor
+
+# Install VSC
+yay -S visual-studio-code-bin
+
 # Install GitHub CLI
 yay -S github-cli
 git config --global user.email "ryangilland@gmail.com"
 git config --global user.name "Ryan Gilland"
 gh auth login
 
-# Install VSC
-yay -S visual-studio-code-bin
-
-# Install Gnome VFS packages
-yay -S gvfs gvfs-afc gvfs-dnssd gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd
-
-# Install workspace packages
-yay -S python-gpgme dropbox slack-desktop libreoffice-fresh
-yay -S nautilus-dropbox
-
-# Install configuration packages
-yay -S extension-manager gnome-tweaks dconf-editor
-
 # gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
-# sudo -u gdm dbus-launch gsettings set org.gnome.login-screen logo '/usr/share/pixmaps/endeavouros-logo-text-dark.svg'
+sudo -u gdm dbus-launch gsettings set org.gnome.login-screen logo '/usr/share/pixmaps/endeavouros-logo-text-dark.svg'
 # sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-theme Adwaita
 
 # Install niri, Hyprland, Astal, AGS, and SWWW packages
 # yay -S niri fuzzel swww
-yay -S libastal-meta aylurs-gtk-shell-git
-yay -S hyprland ags-hyprpanel-git fuzzel swww
-sudo mv $SCRIPT_DIR/../home/userprofile/.config/hypr ~/.config
-sudo mv $SCRIPT_DIR/../home/userprofile/.config/hyprpanel ~/.config
-sudo mv $SCRIPT_DIR/../home/userprofile/.config/fuzzel ~/.config
+# yay -S libastal-meta aylurs-gtk-shell-git
+# yay -S hyprland ags-hyprpanel-git fuzzel swww
+# sudo mv $SCRIPT_DIR/../home/userprofile/.config/hypr ~/.config
+# sudo mv $SCRIPT_DIR/../home/userprofile/.config/hyprpanel ~/.config
+# sudo mv $SCRIPT_DIR/../home/userprofile/.config/fuzzel ~/.config
+sudo mv $SCRIPT_DIR/../home/userprofile/.config/paperwm ~/.config
 
 # Add flags for Electron-native packages to use Wayland
 # echo "--enable-features=WaylandWindowDecorations" > ~/.config/electron-flags.conf
