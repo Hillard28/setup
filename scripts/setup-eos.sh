a9cfb8b8baae0cad3a36ec6d0353bef17a2d0fe9
Ryan Gilland <ryangilland@gmail.com>
2025-03-15 11:43:52 -0400

    Add automation to install scripts

--- scripts/setup-eos.sh
+++ scripts/setup-eos.sh
@@ -2,10 +2,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Update installed packages
-yay -Syyu
+yay -Syyu --noconfirm
 
 # Install missing gnome packages
-yay -S gnome --needed
+yay -Sgq gnome | grep -v evince | yay -S - --needed --noconfirm
+yay -S papers --needed --noconfirm
 
 # Copy background, profile, and icon folders
 sudo mv $SCRIPT_DIR/../home/userprofile/Pictures/Background ~/Pictures
@@ -15,18 +16,18 @@ sudo mv $SCRIPT_DIR/../home/userprofile/.local/share/icons ~/.local/share
 
 # Install Inter and Segoe UI font packages
 sudo mv $SCRIPT_DIR/../home/userprofile/.local/share/fonts ~/.local/share
-yay -S inter-font
+yay -S inter-font --needed --noconfirm
 # yay -S nerd-fonts
 fc-cache -f
 
 # Install and configure ZRAM package
-yay -S zram-generator
+yay -S zram-generator --needed --noconfirm
 sudo sh -c "echo '[zram0]' > /etc/systemd/zram-generator.conf"
 sudo sh -c "echo 'zram-size = ram * 2' >> /etc/systemd/zram-generator.conf"
 sudo sh -c "echo 'compression-algorithm = zstd' >> /etc/systemd/zram-generator.conf"
 
 # Install development libraries
-yay -S base-devel clang llvm cmake openssl zlib xz tk
+yay -S base-devel clang llvm cmake openssl zlib xz tk --needed --noconfirm
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -fsSL https://install.julialang.org | sh
 . ~/.bashrc
@@ -45,34 +46,31 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
 echo 'eval "$(pyenv init - bash)"' >> ~/.profile
 
-# After restarting terminal, run
-# pyenv install 3.12
-# pyenv install 3.13
-
 # Install Stata requirements
-yay -S gtk2 ncurses5-compat-libs
+yay -S gtk2 ncurses5-compat-libs --needed --noconfirm
 
 # Install TeX Live packages 
-yay -S texlive-latexrecommended texlive-latexextra texlive-fontsrecommended texlive-fontsextra texlive-fontutils texlive-formatsextra texlive-xetex texlive-binextra texlive-pictures texlive-mathscience texlive-bibtexextra perl-yaml-tiny perl-file-homedir
+yay -S texlive-latexrecommended texlive-latexextra texlive-fontsrecommended texlive-fontsextra texlive-fontutils texlive-formatsextra texlive-xetex texlive-binextra texlive-pictures texlive-mathscience texlive-bibtexextra perl-yaml-tiny perl-file-homedir --needed --noconfirm
 
 # Install Gnome VFS packages
-yay -S gvfs gvfs-afc gvfs-dnssd gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd
+yay -S gvfs gvfs-afc gvfs-dnssd gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd --needed --noconfirm
 
 # Install workspace packages
-yay -S python-gpgme
-yay -S dropbox
-yay -S slack-desktop
-yay -S libreoffice-fresh
-yay -S nautilus-dropbox
+yay -S python-gpgme --needed --noconfirm
+yay -S dropbox slack-desktop libreoffice-fresh --needed --noconfirm
+yay -S nautilus-dropbox --needed --noconfirm
 
 # Install configuration packages
-yay -S extension-manager gnome-tweaks dconf-editor
+yay -S extension-manager gnome-tweaks dconf-editor --needed --noconfirm
+
+# Install media
+yay -S spotify inkscape --needed --noconfirm
 
 # Install VSC
-yay -S visual-studio-code-bin
+yay -S visual-studio-code-bin --needed --noconfirm
 
 # Install GitHub CLI
-yay -S github-cli
+yay -S github-cli --needed --noconfirm
 git config --global user.email "ryangilland@gmail.com"
 git config --global user.name "Ryan Gilland"
 gh auth login
@@ -90,6 +88,3 @@ sudo -u gdm dbus-launch gsettings set org.gnome.login-screen logo '/usr/share/pi
 # sudo mv $SCRIPT_DIR/../home/userprofile/.config/fuzzel ~/.config
 sudo mv $SCRIPT_DIR/../home/userprofile/.config/paperwm ~/.config
 
-# Add flags for Electron-native packages to use Wayland
-# echo "--enable-features=WaylandWindowDecorations" > ~/.config/electron-flags.conf
-# echo "--ozone-platform-hint=auto" >> ~/.config/electron-flags.conf
