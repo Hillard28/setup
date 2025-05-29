2c8cc785ac85449ccde5333c020aa6a36458467d
Ryan Gilland <ryangilland@gmail.com>
2025-05-29 10:48:33 -0400

    Cleanup configs

--- home/userprofile/.config/ags_niri/widget/utils/Functions.ts
+++
@@ -1,7 +0,0 @@
-import translations from "./translation.json";
-
-export function translate(num: number): string {
-  // @ts-ignore
-  const translation: any = translations.workspaces[num];
-  return translation ? translation : num.toString();
-}
