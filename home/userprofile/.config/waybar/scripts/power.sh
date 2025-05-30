851391cef82924dab4afd424ea9cd1291a03161b
Ryan Gilland <ryangilland@gmail.com>
2025-05-30 10:31:26 -0400

    Update waybar and fuzzel configs

--- home/userprofile/.config/waybar/scripts/power.sh
+++ home/userprofile/.config/waybar/scripts/power.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-SELECTION="$(printf "⏾ Suspend\n⭘ Logout\n⏼ Reboot\n⏻ Shutdown" | fuzzel --dmenu -l 4 --placeholder "Command" -a "top-right" -r 2 -f "Adwaita Mono:size=12" -w 12)"
-
-case $SELECTION in
-	*"Suspend")
-		systemctl suspend;;
-	*"Logout")
-		niri msg action quit --skip-confirmation;;
-	*"Reboot")
-		systemctl reboot;;
-	*"Shutdown")
-		systemctl poweroff;;
-esac
+SELECTION="$(printf "⏾ Suspend\n⭘ Logout\n⏼ Reboot\n⏻ Shutdown" | fuzzel --dmenu -l 4 --placeholder "Command" -a "top-right" -r 2 -f "Adwaita Mono:size=12" -w 12 -B 0)"
+if [ "$SELECTION" ]
+then
+	CONFIRMATION="$(printf "No\nYes" | fuzzel --dmenu -l 2 --placeholder "Suspend?" -a "center" -r 2 -f "Adwaita Mono:size=12" -w 12)"
+	if [ "${CONFIRMATION,,}" = "yes" ]
+	then
+		case $SELECTION in
+			*"Suspend")
+				systemctl suspend;;
+			*"Logout")
+				niri msg action quit --skip-confirmation;;
+			*"Reboot")
+				systemctl reboot;;
+			*"Shutdown")
+				systemctl poweroff;;
+		esac
+	fi
+fi
 
--- home/userprofile/.config/waybar/scripts/power_profile.sh
+++ home/userprofile/.config/waybar/scripts/power_profile.sh
@@ -9,7 +9,7 @@ case $CURRENT in
 	*"performance")
 		CURRENT="Performance";;
 esac
-SELECTION="$(printf " Power Saver\n Balanced\n Performance" | fuzzel --dmenu -l 3 --placeholder "$CURRENT" -a "top-right" -r 2 -f "Adwaita Mono:size=10" -w 14)"
+SELECTION="$(printf " Power Saver\n Balanced\n Performance" | fuzzel --dmenu -l 3 --placeholder "$CURRENT" -a "top-right" -r 2 -f "Adwaita Mono:size=12" -w 14 -B 0)"
 
 case $SELECTION in
 	*"Power Saver")
