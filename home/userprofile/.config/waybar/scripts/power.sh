b1d64e4948a28be6c50f5141cf8d5ee9061b0884
Ryan Gilland <ryangilland@gmail.com>
2025-05-30 13:06:47 -0400

    Update waybar config

--- home/userprofile/.config/waybar/scripts/power.sh
+++ home/userprofile/.config/waybar/scripts/power.sh
@@ -3,7 +3,7 @@
 SELECTION="$(printf "⏾ Suspend\n⭘ Logout\n⏼ Reboot\n⏻ Shutdown" | fuzzel --dmenu -l 4 --placeholder "Command" -a "top-right" -r 2 -f "Adwaita Mono:size=12" -w 12 -B 0)"
 if [ "$SELECTION" ]
 then
-	CONFIRMATION="$(printf "No\nYes" | fuzzel --dmenu -l 2 --placeholder "Suspend?" -a "center" -r 2 -f "Adwaita Mono:size=12" -w 12)"
+	CONFIRMATION="$(printf "No\nYes" | fuzzel --dmenu -l 2 --placeholder "Confirm" -a "center" -r 2 -f "Adwaita Mono:size=12" -w 12)"
 	if [ "${CONFIRMATION,,}" = "yes" ]
 	then
 		case $SELECTION in
