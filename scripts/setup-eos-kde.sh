a4c97a4ef44e418b678a0c11c8b7b66a22f7f728
Ryan Gilland <ryangilland@gmail.com>
2025-07-11 12:35:28 -0400

    Update KDE icon theme

--- scripts/setup-eos-kde.sh
+++ scripts/setup-eos-kde.sh
@@ -45,7 +45,7 @@ echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.pro
 echo 'eval "$(pyenv init - bash)"' >> ~/.profile
 
 # Install Stata requirements
-# yay -S gtk2 ncurses5-compat-libs --needed --noconfirm
+yay -S gtk2 ncurses5-compat-libs --needed --noconfirm
 
 # Install TeX Live packages 
 yay -S texlive-latexrecommended texlive-latexextra texlive-fontsrecommended texlive-fontsextra texlive-fontutils texlive-formatsextra texlive-xetex texlive-binextra texlive-pictures texlive-mathscience texlive-bibtexextra perl-yaml-tiny perl-file-homedir --needed --noconfirm
