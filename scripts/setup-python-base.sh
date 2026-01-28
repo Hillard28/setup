d01dcca680d847af083ea4b3f348f2d784c17630
Ryan Gilland <ryangilland@gmail.com>
2026-01-28 12:01:57 -0500

    Add python initial venv setup script and update noctalia config

---
+++ scripts/setup-python-base.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$USER" = "rgilland" ]; then
+	sudo rm -R ~/.venv/base
+	python -m venv ~/.venv/base
+	source ~/.venv/base/bin/activate
+	python -m pip install pip --upgrade
+	python -m pip install --upgrade black autograd cython datetime duckdb fastexcel geopandas grequests ipykernel ipython jellyfish lifelines linearmodels lxml matplotlib maturin numba numpy openpyxl pandas pillow polars pyarrow pypdf requests scikit-learn scipy selenium statsmodels xlsxwriter
+fi
