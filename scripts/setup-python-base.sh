ab8fb2f6cba9042f8ae03ca37afcc5b53ce0c5a7
Ryan Gilland <ryangilland@gmail.com>
2026-01-29 10:30:36 -0500

    Format python install script

--- scripts/setup-python-base.sh
+++ scripts/setup-python-base.sh
@@ -4,5 +4,36 @@ if [ "$USER" = "rgilland" ]; then
 	python -m venv ~/.venv/base
 	source ~/.venv/base/bin/activate
 	python -m pip install pip --upgrade
-	python -m pip install --upgrade black autograd cython datetime duckdb fastexcel geopandas grequests ipykernel ipython jellyfish lifelines linearmodels lxml mapclassify matplotlib maturin numba numpy openpyxl pandas pillow polars pyarrow pypdf requests scikit-learn scipy selenium statsmodels xlsxwriter --no-cache-dir
+	python -m pip install --upgrade --no-cache-dir \
+		black \
+		autograd \
+		cython \
+		datetime \
+		duckdb \
+		fastexcel \
+		geopandas \
+		grequests \
+		ipykernel \
+		ipython \
+		jellyfish \
+		lifelines \
+		linearmodels \
+		lxml \
+		mapclassify \
+		matplotlib \
+		maturin \
+		numba \
+		numpy \
+		openpyxl \
+		pandas \
+		pillow \
+		polars \
+		pyarrow \
+		pypdf \
+		requests \
+		scikit-learn \
+		scipy \
+		selenium \
+		statsmodels \
+		xlsxwriter
 fi
