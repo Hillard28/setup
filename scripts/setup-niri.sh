c6d18d1865aefc6b98d0ab9b8df7b9e0ab885b7b
Ryan Gilland <ryangilland@gmail.com>
2026-03-04 12:35:30 -0500

    Update niri dependencies

--- scripts/setup-niri.sh
+++ scripts/setup-niri.sh
@@ -11,14 +11,14 @@ cp -r $SCRIPT_DIR/../home/userprofile/.config/niri ~/.config
 yay -S niri xwayland-satellite --needed --noconfirm
 
 # Install noctalia-shell dependencies
-yay -S brightnessctl quickshell cava cliphist wlsunset evolution-data-server --needed --noconfirm
+yay -S brightnessctl imagemagick cava cliphist wlsunset evolution-data-server --needed --noconfirm
 # yay -S ddcutil --needed --noconfirm
 
 # Install noctalia-shell
-yay -S noctalia-shell --needed --noconfirm
+yay -S noctalia-shell noctalia-qs --needed --noconfirm
 
 # Install swayidle
-yay -S swayidle --needed --noconfirm
+# yay -S swayidle --needed --noconfirm
 
 # Install archlinux-xdg-menu for Dolphin compatability
 # yay -S archlinux-xdg-menu --needed --noconfirm
